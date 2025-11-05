5d7cac628525070799a7b5e9433ffbab50f782a1
Lautaro Mendieta <54816650+lautaromendieta@users.noreply.github.com>
2025-11-05 07:46:48 -0300

    14 add zephyr can handler with interrupts (#15)
    * Add new header file
    
    * Add new source file
    
    * Add Zephyr device to constructor
    
    * Fix logging CAN Rx message ID
    
    * Fix setting CAN frame flags
    
    * Update `xferContext` type
    
    * Update `xferContext` type
    
    * Add a valid init value for `xferContext`
    
    * Simplify getting MCAN state
    
    * Add CAN stop before configure and init CAN peripheral
    
    * Add protection for shared access to state
    
    * Update documentation and adjust Rx filters
    
    * Update CAN constructor to receive mode
    
    * Remove hardcoded CANopen filters from CAN handler and add generic fitler add

---
+++ include/mcan_fd_interrupt.h
@@ -0,0 +1,44 @@
+#ifndef MCAN_FD_INTERRUPT_H_
+#define MCAN_FD_INTERRUPT_H_
+
+#include <stdint.h>
+
+#include <zephyr/device.h>
+#include <zephyr/drivers/can.h>
+
+typedef enum {
+  MCAN_MODE_NORMAL,
+  MCAN_MODE_FD_STANDARD,
+  MCAN_MODE_FD_EXTENDED,
+  MCAN_MODE_EXTENDED
+} MCAN_MODE;
+
+typedef enum {
+  APP_STATE_MCAN_RECEIVE,
+  APP_STATE_MCAN_TRANSMIT,
+  APP_STATE_MCAN_IDLE,
+  APP_STATE_MCAN_XFER_SUCCESSFUL,
+  APP_STATE_MCAN_XFER_ERROR,
+  APP_STATE_MCAN_USER_INPUT,
+} APP_STATES;
+
+typedef struct McanFdInterrupt {
+  APP_STATES xferContext;
+  APP_STATES state;
+  uint32_t rxMessageId;
+  uint8_t rxMessage[64];
+  uint8_t rxMessageLength;
+  const struct device *canDev;
+  struct can_frame frame;
+  can_mode_t mode;
+} McanFdInterrupt;
+
+void McanFdInterrupt_new(McanFdInterrupt *self, const struct device *dev, can_mode_t mode);
+bool McanFdInterrupt_receive(McanFdInterrupt *self, uint32_t *rxMessageId, uint8_t *rxMessage, uint8_t *rxMessageLength);
+void McanFdInterrupt_configure(McanFdInterrupt *self);
+bool McanFdInterrupt_send(McanFdInterrupt *self, uint32_t messageId, uint8_t *message, uint8_t messageLength, MCAN_MODE mcanMode);
+uint8_t McanFdInterrupt_getState(McanFdInterrupt *self);
+void McanFdInterrupt_enable(McanFdInterrupt *self);
+bool McanFdInterrupt_addFilter(McanFdInterrupt *self, uint32_t id, uint32_t mask);
+
+#endif  // MCAN_FD_INTERRUPT_H_
