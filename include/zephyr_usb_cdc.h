39818ae25c29ab9be0c7bcbe114457dd1a4e5f97
Lautaro Mendieta <lemendieta96@gmail.com>
2025-11-29 10:13:25 -0300

    Add new header

---
+++ include/zephyr_usb_cdc.h
@@ -0,0 +1,28 @@
+#ifndef ZEPHYR_USB_CDC_H_
+#define ZEPHYR_USB_CDC_H_
+
+#include "communication_interface.h"
+
+#include <zephyr/device.h>
+
+typedef struct ZephyrUsbCdc {
+  CommunicationInterface communicationInterfaceView;
+  const struct device *usbDev;
+} ZephyrUsbCdc;
+
+/**
+ * @brief Constructor for USB CDC structure
+ *
+ * @param self Uninitialized USB CDC structure
+ */
+void ZephyrUsbCdc_create(ZephyrUsbCdc *self);
+
+/**
+ * @brief Returns a USB CDC view as a communication interface
+ * 
+ * @param self Initialized USB CDC structure
+ * @return CommunicationInterface* Communication interface view
+ */
+CommunicationInterface *ZephyrUsbCdc_viewAsCommunicationInterface(ZephyrUsbCdc *self);
+
+#endif  // ZEPHYR_USB_CDC_H_
